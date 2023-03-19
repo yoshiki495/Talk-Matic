cde1b364b02bcae68b347fbb3d7d3fbc12eca392
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2023-03-20 00:48:57 +0900

    タイトルの修正

--- components/LineChat.tsx
+++ components/LineChat.tsx
@@ -74,7 +74,7 @@ const AIChat: React.FC = () => {
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="relative z-10">
-            <h1 className="text-3xl font-bold mb-6">AI Chat</h1>
+            <h1 className="text-3xl font-bold mb-6">Talk Matic</h1>
             <div className="mb-6">
               {isRecording ? (
                 <button
